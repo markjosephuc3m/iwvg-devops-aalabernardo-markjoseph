beb99d91ada0b62bcf29a3d7d38ddb495fd2f9a4
markjosephuc3m <100451329@alumnos.uc3m.es>
2025-09-26 10:50:44 +0200

    Fixed Fraction class issue #5

--- src/main/java/es/upm/miw/devops/code/Fraction.java
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -1,74 +1,65 @@
 package es.upm.miw.devops.code;
 
-import java.util.ArrayList;
-import java.util.List;
+/**
+ * Conceptos: Las fracciones propias son aquellas cuyo numerador es menor que el denominador
+ * <p>
+ * Las fracciones impropias son aquellas cuyo numerador es mayor que el denominador
+ * <p>
+ * Dos fracciones son equivalentes cuando el producto de extremos (numerador de la primera por denominador de la segunda) es igual al
+ * producto de medios (denominador de la primera por el numerador de la segunda)
+ * <p>
+ * Las fracciones irreducibles son aquellas que no se pueden simplificar, esto sucede cuando el numerador y el denominador son primos entre
+ * sí
+ * <p>
+ * Reducir varias fracciones a común denominador consiste en convertirlas en otras equivalentes que tengan el mismo denominador
+ * <p>
+ * Comparar fracciones
+ * <p>
+ * Suma fracciones: En primer lugar se reducen los denominadores a común denominador, y se suman o se restan los numeradores de las
+ * fracciones equivalentes obtenidas
+ * <p>
+ * Multiplicación: La multiplicación de dos fracciones es otra fracción que tiene: Por numerador el producto de los numeradores. Por
+ * denominador el producto de los denominadores.
+ * <p>
+ * La división de dos fracciones es otra fracción que tiene: Por numerador el producto de los extremos. Por denominador el producto de los
+ * medios. Invertir fraccion
+ */
 
 public class Fraction {
-    public static class User {
-        private String id;
-        private String name;
-        private String familyName;
-        private List<Fraction> fractions;
+    private int numerator;
+    private int denominator;
 
-        public User() {
-            this.fractions = new ArrayList<>();
-        }
-
-        public User(String id, String name, String familyName, List<Fraction> fractions) {
-            this.id = id;
-            this.name = name;
-            this.familyName = familyName;
-            this.fractions = fractions;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getFamilyName() {
-            return this.familyName;
-        }
-
-        public void setFamilyName(String familyName) {
-            this.familyName = familyName;
-        }
-
-        public List<Fraction> getFractions() {
-            return fractions;
-        }
-
-        public void setFractions(List<Fraction> fractions) {
-            this.fractions = fractions;
-        }
-
-        public void addFraction(Fraction fraction) {
-            this.fractions.add(fraction);
-        }
-
-        public String fullName() {
-            return this.name + " " + this.familyName;
-        }
-
-        public String initials() {
-            return this.name.charAt(0) + ".";
-        }
-
-        @Override
-        public String toString() {
-            return "User{" +
-                    "id='" + id + '\'' +
-                    ", name='" + name + '\'' +
-                    ", familyName='" + familyName + '\'' +
-                    ", fractions=" + fractions +
-                    '}';
-        }
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
+
+    public Fraction() {
+        this(1, 1);
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" +
+                "numerator=" + numerator +
+                ", denominator=" + denominator +
+                '}';
+    }
+
 }
